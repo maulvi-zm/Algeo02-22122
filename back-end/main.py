1626f6b01475682e13edf0aa867df57ab7cab10b
M Fauzan Azhim <fauzanazz@users.noreply.github.com>
2023-11-05 18:13:01 +0700

    Feat(Bonus) : Webscraper Backend

--- back-end/main.py
+++ back-end/main.py
@@ -4,6 +4,10 @@ from pathlib import Path
 from fastapi.responses import StreamingResponse
 from io import BytesIO
 from fastapi.staticfiles import StaticFiles
+import requests
+from bs4 import BeautifulSoup
+import os
+import caches
 
 UPLOAD_DIR_SEARCH = Path() / "uploads/search"
 UPLOAD_DIR_DATA= Path() / "uploads/data-set"
@@ -35,7 +39,7 @@ async def create_upload_file(file_upload: UploadFile):
 
 @app.post("/uploaddata/")
 async def create_upload_file(file_uploads: list[UploadFile]):
-
+    delete_dataset()
     for file_upload in file_uploads:
         contents = await file_upload.read()
         save_path = UPLOAD_DIR_DATA / file_upload.filename
@@ -44,12 +48,43 @@ async def create_upload_file(file_uploads: list[UploadFile]):
         
     return {"filenames": [f.filename for f in file_uploads]}
 
+
 @app.post("/capture_frame")
 async def receive_image(file: UploadFile = File(...)):
     content = await file.read()
     save_path = UPLOAD_DIR_SEARCH / "received_image.jpg"
-    # Write the received image content to a file
     with open(save_path, "wb") as f:
         f.write(content)
-
+        
     return {"message": "Image received and processed"}
+
+
+@app.get("/scrape")
+async def scrape_images(link: str):
+    delete_dataset()
+    response = requests.get(link)
+    if response.status_code == 200:
+        
+        soup = BeautifulSoup(response.content, 'html.parser')
+        image_tags = soup.find_all('img')
+        os.makedirs(UPLOAD_DIR_DATA, exist_ok=True)
+        
+        for index, img in enumerate(image_tags):
+            img_link = img.get('src')
+            
+            if img_link:
+                img_response = requests.get(img_link)
+                
+                if img_response.status_code == 200:
+                    with open(os.path.join(UPLOAD_DIR_DATA, f"{index}.jpg"), 'wb') as f:
+                        f.write(img_response.content)
+    else:
+        print("Failed to fetch the webpage")
+
+    return {"message": "Images scraped and saved to uploads/data-set"}
+
+def delete_dataset():
+    for file in os.listdir(UPLOAD_DIR_DATA):
+        os.remove(UPLOAD_DIR_DATA / file)
+        
+    return {"message": "Data-set deleted"}
